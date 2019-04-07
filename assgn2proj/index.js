6393fdc52fa0f06ed9f56be8f4b26e3e8f592029
Apurva Gawad <gawadapurva@gmail.com>
2019-04-07 14:59:29 +0100

    version 2

---
+++ assgn2proj/index.js
@@ -0,0 +1,21 @@
+let http = require('http');
+let fs = require('fs');
+ 
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./index.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+};
+ 
+const port = process.env.PORT || 8000;
+http.createServer(handleRequest).listen(port);
+console.log("Server running on port 8000");
