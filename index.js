aec5fa6b0c428b398f04e4a0e5f35b4096691599
Tejas Hande <tejas.hande2@mail.dcu.ie>
2019-04-08 14:11:04 +0100

    Modifying code to read newly created index.html page

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let http = require('http');
 let fs = require('fs');
- 
+let path = require('path');
+
 let handleRequest = (request, response) => {
     response.writeHead(200, {
         'Content-Type': 'text/html'
     });
-    fs.readFile('./index.html', null, function (error, data) {
+    fs.readFile('./assgn2proj/index.html', null, function (error, data) {
         if (error) {
             response.writeHead(404);
             respone.write('Whoops! File not found!');
@@ -15,7 +16,16 @@ let handleRequest = (request, response) => {
         response.end();
     });
 };
- 
+
 const port = process.env.PORT || 8000;
-http.createServer(handleRequest).listen(port);
+var nStatic = require('node-static');
+var fileServer = new nStatic.Server('./assgn2proj');
+http.createServer(function (req, res) {
+    
+    fileServer.serve(req, res);
+
+}).listen(port);
+
+// console.log(path.dirname(require.main.filename));
+// http.createServer(handleRequest).listen(port);
 console.log("Server running on port 8000");
